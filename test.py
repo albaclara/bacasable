b17b6101c61e169fbeedd3c01f508fc25deaf6c4
Eve Séguier <eve.seguier@laposte.net>
2018-05-13 16:31:46 +0200

    modif test

--- test.py
+++ test.py
@@ -1,9 +1,2 @@
 # -*-coding:Utf-8 -*
 import unicodedata 
-
-villenormalisee = unicodedata.normalize('NFD', 'vert').encode('ascii', 'ignore') 
-
-print(villenormalisee)
-villenormalisee = unicodedata.normalize('NFD', 'vèrt').encode('ascii', 'ignore') 
-
-print(villenormalisee)
